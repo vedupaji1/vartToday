6e613a9fee02d9d1a0f55a43623b083cb3ab4017
vedupaji1 <85487800+vedupaji1@users.noreply.github.com>
2022-03-30 23:30:11 +0530

    Add files via upload

---
+++ client/src/Components/Loading.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Loading = () => {
+    return (
+        <>
+            <div className="mainLoader">
+                <div className="subLoader">
+                    <div className="loaderText">V</div>
+                </div>
+                <div className="animatorDiv"></div>
+            </div>
+        </>
+    )
+}
+
+export default Loading;
