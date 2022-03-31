6e6562def5939026397f21d4c5477ed425d5f011
vedupaji1 <85487800+vedupaji1@users.noreply.github.com>
2022-03-31 09:08:30 +0530

    Add files via upload

---
+++ client/src/Components/Error404.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Error404 = () => {
+    return (
+        <>
+            <div className="div1">
+                <div className="subMainDivError404">
+                    <div className="div2">
+                        <div className="op40 err1">
+                            <h1>4</h1>
+                        </div>
+                        <div className="op41 err1">
+                            <h1>0</h1>
+                        </div>
+                        <div className="op42 err1">
+                            <h1>4</h1>
+                        </div>
+                    </div>
+                    <div className="message1">
+                        <h1>Something Went Wrong</h1>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Error404;
